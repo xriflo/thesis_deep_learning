734f1e012d0fc35da058be3b2fdd350922c8ab4b
Florentina Bratiloveanu <florentina.bratiloveanu@gmail.com>
2015-09-03 22:05:26 +0300

    added tansf functions

---
+++ diploma/src/appendix/model.lua
@@ -0,0 +1,15 @@
+model = nn.Sequential()
+      
+model:add(nn.SpatialConvolutionMM(3, 8, 5, 5))
+model:add(nn.Tanh())
+model:add(nn.SpatialSubSampling(8, 2, 2, 2, 2))
+
+model:add(nn.SpatialConvolutionMM(8, 20, 5, 5))
+model:add(nn.Tanh())
+model:add(nn.SpatialSubSampling(20, 2, 2, 2, 2))
+
+model:add(nn.SpatialConvolutionMM(20, 120, 5, 5))
+model:add(nn.Reshape(120))
+model:add(nn.Linear(120, 100))
+model:add(nn.Tanh())
+model:add(nn.Linear(100, 4))
