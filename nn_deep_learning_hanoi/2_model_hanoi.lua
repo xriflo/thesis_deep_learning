8570bb360d70f082b225442870d207b6bec0c60c
Florentina Bratiloveanu <florentina.bratiloveanu@gmail.com>
2015-09-08 23:06:23 +0300

    original model

--- nn_deep_learning_hanoi/2_model_hanoi.lua
+++ nn_deep_learning_hanoi/2_model_hanoi.lua
@@ -10,17 +10,23 @@ height = 32
 ninputs = nfeats * width * height
 nhiddens = ninputs / 2
 
+nstates = {8,20,120,100}
+filtsize = 5
+poolsize = 2
+normkernel = image.gaussian1D(7)
+
 model = nn.Sequential()
+
 model:add(nn.SpatialConvolutionMM(3, 8, 5, 5))
-model:add(nn.ReLU())
+model:add(nn.Tanh())
 model:add(nn.SpatialSubSampling(8, 2, 2, 2, 2))
 
 model:add(nn.SpatialConvolutionMM(8, 20, 5, 5))
-model:add(nn.ReLU())
+model:add(nn.Tanh())
 model:add(nn.SpatialSubSampling(20, 2, 2, 2, 2))
 
-model:add(nn.SpatialConvolutionMM(20, 16, 5, 5))
-model:add(nn.Reshape(16))
-model:add(nn.Linear(16, 8))
-model:add(nn.ReLU())
-model:add(nn.Linear(8, 4))	
+model:add(nn.SpatialConvolutionMM(20, 120, 5, 5))
+model:add(nn.Reshape(120))
+model:add(nn.Linear(120, 100))
+model:add(nn.Tanh())
+model:add(nn.Linear(100, 4))
